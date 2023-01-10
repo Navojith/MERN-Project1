224c169a52c02cc2e38795b19bc948c936ec2576
Navojith <navojith22@gmail.com>
2023-01-10 20:17:27 +0530

    display all

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')       //should update later before release lesson 9
+            const json = await response.json()
+
+            if(response.ok) {
+                setWorkouts(json)
+            }
+        } 
+        
+        fetchWorkouts()
+    }, []) // [] is use so that ith function only fires one time, not every time the component loads
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
         </div>
     )
 }
